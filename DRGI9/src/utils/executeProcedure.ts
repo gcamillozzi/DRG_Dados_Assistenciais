ee7511521e2aee1e53d4f46e808f9b0bad2759e1
gcamillozzi <gustavo.camillozzi@gmail.com>
2024-07-26 10:27:39 -0300

    Update executeProcedure.ts

--- DRGI9/src/utils/executeProcedure.ts
+++ DRGI9/src/utils/executeProcedure.ts
@@ -1,4 +1,4 @@
 import knex from '../config/database'
 export async function executeProcedure() {
-  await knex.raw(`BEGIN DATAINTEGRA.PRC_DTI_DIARIA(); END;`)
+  await knex.raw(`BEGIN INM.PRC_INM_DIARIA(); END;`)
 }
